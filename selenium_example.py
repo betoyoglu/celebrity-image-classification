9b2eaa03450838216183f94814d7b8a01d783a20
betoyoglu <201805054@stu.adu.edu.tr>
2024-07-15 13:36:27 +0300

    update

--- selenium_example.py
+++ selenium_example.py
@@ -15,25 +15,26 @@ search_box= driver.find_element(By.CSS_SELECTOR, '.gLFyf[name="q"]').send_keys("
 time.sleep(5)
 
 # Resimlerin dinamik olarak yüklenmesini sağlamak için sayfayı aşağı kaydır
-for _ in range(3):  # 3 kez aşağı kaydırarak daha fazla resim yükleyin
+for _ in range(2): 
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(2)  # Sayfanın yüklenmesini bekleyin
+    time.sleep(2) 
 
 # Resim elementlerini bulun
-image_elements = driver.find_elements(By.CSS_SELECTOR, '.czzyk XOEbc')
+image_elements = driver.find_elements(By.CSS_SELECTOR, '.YQ4gaf img[src]')
 
 # İlk 5 resim URL'sini al ve indir
 for index, img in enumerate(image_elements[:5]):
     try:
-        img_url = img.get_attribute('g-img')
-        if img_url is None:
-            img_url = img.get_attribute('data-src')
+        img_url = img.get_attribute('src')
         if img_url:
-            response = requests.get(img_url)
-            with open(f"taeyong{index}.jpg", 'wb') as f:
-                f.write(response.content)
+            response = requests.get(img_url, stream=True)
+            if response.status_code == 200:
+                with open(f"taeyong{index}.jpg", 'wb') as f:
+                    for chunk in response.iter_content(1024):
+                        f.write(chunk)
+            else:
+                print(f"Error downloading image {img_url}: status code {response.status_code}")
     except Exception as e:
-        print(f"Hata oluştu: {e}")
+        print(f"Error: {e}")
 
-# WebDriver'ı kapat
 driver.quit()
