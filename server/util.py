8557de001fd4bf3e7e97af156a409ac56d314d9c
betoyoglu <201805054@stu.adu.edu.tr>
2024-07-17 12:07:09 +0300

    update

--- server/util.py
+++ server/util.py
@@ -12,8 +12,8 @@ __class_number_to_name = {}
 __model = None
 
 def get_cropped_image_if_2_eyes(image_path, image_base64_data):
-    face_cascade = cv2.CascadeClassifier(".\opencv\haarcascade_frontalface_default.xml")
-    eye_cascade = cv2.CascadeClassifier(".\opencv\haarcascade_eye.xml")
+    face_cascade = cv2.CascadeClassifier(u".\opencv\haarcascade_frontalface_default.xml")
+    eye_cascade = cv2.CascadeClassifier(u".\opencv\haarcascade_eye.xml")
     if image_path:
         img = cv2.imread(image_path)
     else:
@@ -59,13 +59,13 @@ def load_saved_artifacts():
     global __class_name_to_number
     global __class_number_to_name
 
-    with open("C:\\Users\\Betül\\OneDrive\\Desktop\\örnekler\\a_örneksssss\\sports-celebrity-image-classification\\server\\artifacts\\class_dictionary.json", "r") as f:
+    with open("class_dictionary.json", "r") as f:
         __class_name_to_number = json.load(f)
         __class_number_to_name = {v:k for k,v in __class_name_to_number.items()}
 
     global __model
     if __model is None:
-        with open("C:\\Users\\Betül\\OneDrive\\Desktop\\örnekler\\a_örneksssss\\sports-celebrity-image-classification\\server\\artifacts\\saved_model.pkl", "rb") as f:
+        with open("saved_model.pkl", "rb") as f:
             __model = joblib.load(f)
     print("loading saved artifacts... done")
 
@@ -79,14 +79,14 @@ def get_cv2_image_from_base64_string(b64str):
     return img
 
 def get_b64_test_image_for_virat():
-    with open("C:\\Users\\Betül\\OneDrive\\Desktop\\örnekler\\a_örneksssss\\sports-celebrity-image-classification\\server\\baek.txt") as f:
+    with open("./server/baek.txt") as f:
         return f.read()
     
 if __name__ == "__main__":
     load_saved_artifacts()
     #print(classify_image(get_b64_test_image_for_virat(), None))
 
-    print(classify_image(None, "C:\\Users\\Betül\\OneDrive\\Desktop\\örnekler\\a_örneksssss\\sports-celebrity-image-classification\\server\\test_images\\mark_0.jpg"))
-    print(classify_image(None, "C:\\Users\\Betül\\OneDrive\\Desktop\\örnekler\\a_örneksssss\\sports-celebrity-image-classification\\server\\test_images\\seulgi_6.jpg"))
-    print(classify_image(None, "C:\\Users\\Betül\\OneDrive\\Desktop\\örnekler\\a_örneksssss\\sports-celebrity-image-classification\\server\\test_images\\taeyong_4.jpg"))
-    print(classify_image(None, "C:\\Users\\Betül\\OneDrive\\Desktop\\örnekler\\a_örneksssss\\sports-celebrity-image-classification\\server\\test_images\\yunjin_1.jpg"))
+    print(classify_image(None, "mark_0.jpg"))
+    print(classify_image(None, "seulgi_6.jpg"))
+    print(classify_image(None, "taeyong_4.jpg"))
+    print(classify_image(None, "yunjin_1.jpg"))
