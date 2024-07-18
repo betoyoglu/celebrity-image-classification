7092737c6d96bbf16883c519bd31f8b190a58cb3
betoyoglu <201805054@stu.adu.edu.tr>
2024-07-18 15:25:02 +0300

    update
    proje tamamlandı

--- server/server.py
+++ server/server.py
@@ -3,16 +3,14 @@ import util
 
 app = Flask(__name__)
 
-@app.route('/classify_image', methods= ['GET', 'POST'])
+@app.route('/classify_image', methods=['POST'])
 def classify_image():
-    image_data = request.form["image_data"] #base64 olucak
-
-    response = jsonify(util.classify_image(image_data)) #convert to json
-
+    image_data = request.form["image_data"]  # base64 string
+    response = jsonify(util.classify_image(image_data))  # convert to json
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
 if __name__ == "__main__":
     print("starting python flask server for celebrity image classification")
-    util.load_saved_artifacts()
+    util.load_saved_artifacts()  # assuming this function loads your model or other necessary artifacts
     app.run(port=5000)
